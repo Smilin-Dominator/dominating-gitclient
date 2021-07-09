f59f5908130650087bdf49f455d43fd7f6e48777
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-07-09 12:08:14 +0530

    Added A Committing Function

--- commit.py
+++ commit.py
@@ -18,6 +18,7 @@ def main():
             case "1":
                 mfilelist = []
                 nfilelist = []
+                dfilelist = []
                 a = subprocess.check_output("git status").decode().splitlines()
                 for supposed_file in a:
                     if supposed_file.startswith("\tnew file:"):
@@ -28,10 +29,25 @@ def main():
                         name = supposed_file.replace(" ", "").replace("modified:", '')
                         string = "\tModified File: " + name
                         mfilelist.append(string)
+                    elif supposed_file.startswith("\tdeleted: "):
+                        name = supposed_file.replace(" ", "").replace("deleted:", '')
+                        string = "\tDeleted File: " + name
+                        dfilelist.append(string)
                 print("\nNew Files: \n")
                 for file in nfilelist:
                     print(file)
                 print("\nModified Files: \n")
                 for file in mfilelist:
                     print(file)
+                print("\nDeleted Files: \n")
+                for file in dfilelist:
+                    print(file)
+                print("\n\n[*] Which Files Would You Like To Commit? (all / [enter the files sep. by spaces]")
+                comlist = input("[*] ")
+                if comlist == "all":
+                    print(f"\n{subprocess.check_output('git commit *')}\n")
+                else:
+                    comlist = comlist.split(" ")
+                    cmd = f"git commit {' '.join(comlist)}"
+                    print(f"""\n{subprocess.check_output(cmd)}\n""")
                 enterprompt()
