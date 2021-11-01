9092678c17d9e997a9d622ee188f4c7657dc3e45
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-01 23:57:33 +0530

    Ported rich IO prompts from mspb

---
+++ config.py
@@ -0,0 +1,50 @@
+from rich.console import Console
+from rich.prompt import Prompt
+import logging
+
+log_format = '%(asctime)s (%(filename)s): %(message)s'
+logging.basicConfig(filename="log.txt", format=log_format)
+
+console = Console(color_system="256")
+
+def print(prompt, override: str = None) -> None:
+    if override is not None:
+        console.print(f"[{override}]{prompt}[/{override}]")
+    else:
+        console.print(f"{prompt}")
+
+
+def error(msg: str, override: str = None) -> None:
+    if override is None:
+        print(f"[white on red][@] {msg}[/white on red]")
+    else:
+        print(f"[{override}][@] {msg}[/{override}]")
+    logging.error(f"{msg}\nLocals: {locals()}")
+
+
+def warning(msg: str, override: str = None) -> None:
+    if override is None:
+        print(f"[white on yellow][!] {msg}[/white on yellow]")
+    else:
+        print(f"[{override}][!] {msg}[/{override}]")
+    logging.warning(msg)
+
+
+def info(msg: str, override: str = None) -> None:
+    if override is not None:
+        print(f"[{override}][?] {msg}[/{override}]")
+    else:
+        print(f"[?] {msg}")
+
+
+def input(prompt: str, override: str = None, default=None, password=False) -> str:
+    if default is None:
+        if override is not None:
+            return Prompt.ask(f"[{override}]{prompt}[/{override}]", password=password)
+        else:
+            return Prompt.ask(f"{prompt}", password=password)
+    else:
+        if override is not None:
+            return Prompt.ask(f"[{override}]{prompt}[/{override}]", default=default, password=password)
+        else:
+            return Prompt.ask(f"{prompt}", default=default, password=password
