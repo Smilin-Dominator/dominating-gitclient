6b2e5d4887317182cffbfa452cbd053a068bb5f8
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-02 00:26:13 +0530

    Made main change directories and fixed repo detection

--- __main__.py
+++ __main__.py
@@ -1,15 +1,22 @@
 from subprocess import getoutput
 from client.first_time import main as setup
 from client.main import main as cl
+from config import warning
+from sys import argv, exit
+from os import chdir
 
 
 def first_time_check():
-    key = bool(getoutput("git rev-parse --is-inside-work-tree"))
-    if not key:
+    key = getoutput("git rev-parse --is-inside-work-tree")
+    if key == "fatal: not a git repository (or any of the parent directories): .git":
         setup()
 
 
 if __name__ == "__main__":
+    if len(argv) < 2:
+        warning("Not Enough Arguments")
+        exit(1)
+    chdir(argv[1])
     first_time_check()
     cl()
 
