384d2f576773f89f8f22b25780e1c991eb234e95
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-02 00:42:55 +0530

    Added first-time setup

--- client/first_time.py
+++ client/first_time.py
@@ -1,2 +1,16 @@
+from config import info, input, error
+from subprocess import call
+from sys import exit
+
 def main():
-    print("")
+    info("There's no git repository in the specified path")
+    choice = input("Create One?", override="navajo_white3", default="y", choices=["y", "n"])
+    match choice:
+        case "y":
+            code = call("git init", shell=True)
+            if code != 0:
+                error("Error While Running Git Init")
+            else:
+                info("Success!")
+        case "n":
+            exit(0)
