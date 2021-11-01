8d035ebde8d58182abc34c31661dd70cc9fe9407
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-02 00:51:39 +0530

    Foundation for main interface

--- client/main.py
+++ client/main.py
@@ -1,5 +1,35 @@
-from config import header
+from sys import exit
+from config import header, print, input, error, warning
+from .commit import main as com
 
 
 def main():
     header()
+    print("""
+        1) Commit Functions
+        2) Branch Functions
+        3) Remote Management
+        4) Stash Management
+        5) Commit Log
+        ..
+        99) Quit
+    """)
+    try:
+        choice = int(input("\td"))
+        match choice:
+            case 1:
+                com()
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 99:
+                exit(0)
+            case _:
+                warning("Number doesn't match")
+    except ValueError:
+        error("Didn't enter an integer during the main selection")
