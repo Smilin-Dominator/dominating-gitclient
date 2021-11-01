3cfeea2338d4869343760c8d736f2e9339b5aa55
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-01 12:30:47 +0530

    Initial

--- commit.sh
+++
@@ -1,5 +0,0 @@
-git log \
-    --pretty=format:'{%n  "_id": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%f"%n},' \
-    $@ | \
-    perl -pe 'BEGIN{print "["}; END{print "]\n"}' | \
-    perl -pe 's/},]/}]/'
