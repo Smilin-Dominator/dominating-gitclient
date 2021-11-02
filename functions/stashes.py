800c6e6c39ae770fc69de4ab48d24bbce3b0816e
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-02 20:39:09 +0530

    Fixed the error of not getting an index

--- functions/stashes.py
+++ functions/stashes.py
@@ -5,5 +5,5 @@ def get_index(message: str) -> int:
     stash = getoutput("git stash list").splitlines()
     for line in stash:
         line = line.split(": ")
-        if line[2] == message:
-            return int(line[1].strip("stash@{}"))
+        if line[2].strip("'") == message:
+            return int(line[0].strip("stash@{}"))
