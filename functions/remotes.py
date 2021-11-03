3a8e05694e687019b7d893cac7fbe1bc4e563fbc
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-03 23:30:11 +0530

    Added a branch describing function

--- functions/remotes.py
+++ functions/remotes.py
@@ -1,4 +1,4 @@
-from config import header, warning, input, print, get_branch, enter_prompt
+from config import header, warning, input, print, get_branch, enter_prompt, info
 from subprocess import getoutput, call, DEVNULL
 
 
@@ -54,20 +54,34 @@ def manage_remotes():
     def change_remote_url():
         while True:
             try:
-                branch = input("\tName Of The Branch?", choices=set_header("g"), override="indian_red1")
+                remote = input("\tName Of The Remote?", choices=set_header("g"), override="indian_red1")
                 new = input("\tNew URL", override="deep_pink2")
                 if " " in new:
                     warning("You Cannot Have Spaces In A Remote URL!")
                     continue
-                call(f"git remote set-url {branch} '{new}'")
+                call(f"git remote set-url {remote} '{new}'")
                 break
             except KeyboardInterrupt:
                 break
 
     def delete_remote():
         try:
-            branch = input("\tName Of The Branch?", choices=set_header("g"), override="indian_red1")
-            call(f"git remote remove {branch}")
+            remote = input("\tName Of The Remote?", choices=set_header("g"), override="indian_red1")
+            call(f"git remote remove {remote}")
+        except KeyboardInterrupt:
+            pass
+
+    def describe_remote():
+        try:
+            remote = input("\tName Of The Remote?", choices=set_header("g"), override="indian_red1")
+            branches = getoutput(f"git ls-remote --heads {remote}").splitlines()
+            info(f"Remote Name: [khaki3]{remote}[/khaki3]")
+            info(f"Remote URL: {getoutput(f'git remote get-url {remote}')}")
+            branch_list = []
+            for branch in branches:
+                branch_list.append(branch.split("\t")[1][11:])
+            info(f"Branches: '{', '.join(branch_list)}'")
+            enter_prompt()
         except KeyboardInterrupt:
             pass
 
@@ -77,10 +91,11 @@ def manage_remotes():
         2) Rename A Remote
         3) Change A Remote's URL
         4) Delete A Remote
+        5) Describe A Remote
         """
     )
     try:
-        choice = int(input("\tChoice", override="light_steel_blue1", choices=["1", "2", "3", "4"]))
+        choice = int(input("\tChoice", override="light_steel_blue1", choices=["1", "2", "3", "4", "5"]))
         match choice:
             case 1:
                 add_remote()
@@ -90,6 +105,8 @@ def manage_remotes():
                 change_remote_url()
             case 4:
                 delete_remote()
+            case 5:
+                describe_remote()
     except TypeError:
         pass
     except KeyboardInterrupt:
