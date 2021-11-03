91d5cd4466ba50c43f3c2ec453ae6698884d2585
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-03 10:50:10 +0530

    Added push and pull and a few other functions to assist

--- client/remote.py
+++ client/remote.py
@@ -1,2 +1,30 @@
+from functions.remotes import *
+from config import print
+
+
 def main():
-    pass
+    while True:
+        set_header()
+        print("""
+        1) Push
+        2) Pull
+        3) Fetch
+        4) Manage Remotes
+        ..
+        99) Previous Menu
+            """)
+        try:
+            choice = int(input("\tChoice", choices=["1", "2", "3", "4", "5", "99"]))
+            match choice:
+                case 1:
+                    push()
+                case 2:
+                    pull()
+                case 3:
+                    pass
+                case 4:
+                    pass
+                case 99:
+                    break
+        except TypeError:
+            pass
--- functions/remotes.py
+++ functions/remotes.py
@@ -1,13 +1,71 @@
-from config import header
+from config import header, warning, input, print, get_branch, enter_prompt
+from subprocess import getoutput, call
 
 
-def set_header():
+def set_header(get=None):
+    remotes = getoutput('git remote').splitlines()
+    if get is None:
+        header(
+            f"[light_steel_blue1][ Remotes: '{', '.join(remotes)}' ][/light_steel_blue1]"
+        )
+    else:
+        return remotes
+
+
+def set_branch_upstream(branch=None, remote=None):
+    if branch and remote is None:
+        pass
+    else:
+        call(f"git push --set-upstream {remote} {branch}", shell=True)
+
+
+def manage_remotes():
     pass
 
 
 def pull():
-    pass
+    try:
+        remotes = set_header("get")
+        if len(remotes) == 0:
+            warning("You Don't Have A Remote!")
+            conf = input("Would you like to setup one?", choices=["y", "n"])
+            if conf == "y":
+                manage_remotes()
+        elif len(remotes) == 1:
+            log = getoutput(f"git pull")
+            print(log)
+            if log.startswith("fatal: couldn't find remote ref"):
+                warning("Your branch doesn't have an upstream branch")
+                conf = input("Would you like to set it upstream?", choices=["y", "n"])
+                if conf == "y":
+                    set_branch_upstream(get_branch(), remotes[0])
+        else:
+            remote = input("Which Remote Do You Want To Pull From?", choices=remotes, override="dark_orange")
+            call(f"git pull {remote} {get_branch()}")
+        enter_prompt()
+    except KeyboardInterrupt:
+        pass
 
 
 def push():
-    pass
+    try:
+        remotes = set_header("get")
+        if len(remotes) == 0:
+            warning("You Don't Have A Remote!")
+            conf = input("Would you like to setup one?", choices=["y", "n"])
+            if conf == "y":
+                manage_remotes()
+        elif len(remotes) == 1:
+            log = getoutput(f"git push")
+            print(log)
+            if log.startswith("fatal: couldn't find remote ref"):
+                warning("Your branch doesn't have an upstream branch")
+                conf = input("Would you like to set it upstream?", choices=["y", "n"])
+                if conf == "y":
+                    set_branch_upstream(get_branch(), remotes[0])
+        else:
+            remote = input("Which Remote Do You Want To Pull From?", choices=remotes, override="dark_orange")
+            call(f"git pull {remote} {get_branch()}")
+        enter_prompt()
+    except KeyboardInterrupt:
+        pass
