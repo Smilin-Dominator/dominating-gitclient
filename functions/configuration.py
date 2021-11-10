7b26ee5115123bb0bc92a064bb84dca91fe7ab9b
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-10 21:57:10 +0530

    Finished write_config() Added an inner class 'WriteJSON' and made custom fields for it.

--- functions/configuration.py
+++ functions/configuration.py
@@ -10,7 +10,15 @@ class Config(object):
         self.file = Path(file, "config.json")
 
     def write_config(self) -> None:
-        pass
+        config = self.WriteJSON()
+        config.__add__("field1", "a")
+        config.__add__("field2", "b")
+        config.__add__("field3", "c")
+        config_file = open(self.file, "w+")
+        json_dump = dumps(config.__get__(), indent=4)
+        config_file.write(json_dump)
+        config_file.flush()
+        config_file.close()
 
     def parse_config(self) -> dict:
         try:
@@ -19,3 +27,14 @@ class Config(object):
         except FileNotFoundError:
             warning("No Config File Found, Making One")
             self.write_config()
+
+    class WriteJSON(object):
+
+        def __init__(self):
+            self.config = {}
+
+        def __add__(self, field: str, value):
+            self.config[field] = value
+
+        def __get__(self):
+            return self.config
