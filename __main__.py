0fe07e264b56255032c5332293765f338d3ffd6f
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-10 23:25:14 +0530

    updated the auto-update system - Made a function write which accepts a path object and contents to write - does 'git fetch' and 'git status' instead of the previous

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ from subprocess import getoutput
 from client.first_time import main as setup
 from client.main import main as cl
 from functions.configuration import Config
-from config import warning, error, info, input
+from config import warning, error, info, input, enter_prompt
 from sys import argv, exit
 from os import chdir
 from subprocess import call, DEVNULL
@@ -16,34 +16,37 @@ def first_time_check():
 
 
 def config():
+
+    def write(file: Path, contents: str):
+        fil = open(file, "w")
+        fil.write(contents)
+        fil.flush()
+        fil.close()
+
     conf = Config()
     ar = conf.parse_config()
     if ar['update_check_frequency'] != 0:
         number = ar['update_check_frequency']
         count = Path(str(conf.file)[:-11], "count.txt")
         try:
-            with open(count, "r+") as b:
+            with open(count, "r") as b:
                 num = int(b.read())
                 if num == number:
-                    lines = getoutput("git remote update").splitlines()
-                    if len(lines) == 1:
-                        info("No Update Needed")
+                    call("git fetch origin", shell=True, stdout=DEVNULL)
+                    lines = getoutput("git status").splitlines()
+                    if not lines[1].startswith("Your branch is behind"):
+                        info("Program is up to date")
+                        enter_prompt()
                     else:
                         up = input("\tUpdate Found, Would You Like To Update?", override="red", choices=["y", "n"], default="y")
                         if up:
                             call("git pull origin master", shell=True)
-                    b.write("1")
-                    b.flush()
-                    b.close()
+                    write(count, "1")
                 else:
-                    b.write(str(num + 1))
-                    b.flush()
-                    b.close()
+                    write(count, str(num + 1))
         except FileNotFoundError:
             with open(count, "w") as b:
-                b.write("1")
-                b.flush()
-                b.close()
+                write(count, "1")
 
 
 if __name__ == "__main__":
