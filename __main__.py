bc16b90cefa3f5bb04db311e202299f156198c9c
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-10 23:28:59 +0530

    Shifted update() to its own function

--- __main__.py
+++ __main__.py
@@ -17,15 +17,7 @@ def first_time_check():
 
 def config():
 
-    def write(file: Path, contents: str):
-        fil = open(file, "w")
-        fil.write(contents)
-        fil.flush()
-        fil.close()
-
-    conf = Config()
-    ar = conf.parse_config()
-    if ar['update_check_frequency'] != 0:
+    def update():
         number = ar['update_check_frequency']
         count = Path(str(conf.file)[:-11], "count.txt")
         try:
@@ -38,7 +30,8 @@ def config():
                         info("Program is up to date")
                         enter_prompt()
                     else:
-                        up = input("\tUpdate Found, Would You Like To Update?", override="red", choices=["y", "n"], default="y")
+                        up = input("\tUpdate Found, Would You Like To Update?", override="red", choices=["y", "n"],
+                                   default="y")
                         if up:
                             call("git pull origin master", shell=True)
                     write(count, "1")
@@ -47,6 +40,17 @@ def config():
         except FileNotFoundError:
             with open(count, "w") as b:
                 write(count, "1")
+
+    def write(file: Path, contents: str):
+        fil = open(file, "w")
+        fil.write(contents)
+        fil.flush()
+        fil.close()
+
+    conf = Config()
+    ar = conf.parse_config()
+    if ar['update_check_frequency'] != 0:
+        update()
 
 
 if __name__ == "__main__":
