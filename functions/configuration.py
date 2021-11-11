68167abf05deff149d1b20f1c91acad44bd587a4
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-11-11 09:36:33 +0530

    Fixed parse_config() - Made it a while loop that returns the object

--- functions/configuration.py
+++ functions/configuration.py
@@ -38,12 +38,13 @@ class Config(object):
         config_file.close()
 
     def parse_config(self) -> dict:
-        try:
-            config_file = open(self.file, "r")
-            return loads(config_file.read())
-        except FileNotFoundError:
-            warning("No Config File Found, Making One")
-            self.write_config()
+        while True:
+            try:
+                config_file = open(self.file, "r")
+                return loads(config_file.read())
+            except FileNotFoundError:
+                warning("No Config File Found, Making One")
+                self.write_config()
 
     class WriteJSON(object):
 
